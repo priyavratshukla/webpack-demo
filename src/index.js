dc422b4787a322237ca2a5129260c2108ec534b8
Shukla, Priyavrat <priyavrat.shukla@bell.ca>
2018-12-18 19:16:29 +0530

    assest management in webpack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+import jsonData from './data.json';
 function component() {
   let element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+	var myIcon = new Image();
+   	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
+
+	console.log(Data);
+	console.log(jsonData);
 
   return element;
 }
