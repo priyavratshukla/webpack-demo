b0ee07ea545d011af0422c5c6a7540c1d8d714aa
Shukla, Priyavrat <priyavrat.shukla@bell.ca>
2018-12-19 12:44:39 +0530

    output management in webpack

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
 import jsonData from './data.json';
+import printMe from './print.js';
 function component() {
   let element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+  
 
   // Add the image to our existing div.
 	var myIcon = new Image();
@@ -18,6 +21,10 @@ function component() {
 
 	console.log(Data);
 	console.log(jsonData);
+
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
+	element.appendChild(btn);
 
   return element;
 }
